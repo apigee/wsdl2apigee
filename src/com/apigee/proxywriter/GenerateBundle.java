e0e814936f474c130cdd585f59322cc3e5565616
Srinandan Sridhar <nandanks@gmail.com>
2016-07-14 14:45:02 -0400

    clean up code

--- src/com/apigee/proxywriter/GenerateBundle.java
+++ src/com/apigee/proxywriter/GenerateBundle.java
@@ -30,99 +30,8 @@ public class GenerateBundle {
 		LOGGER.addHandler(handler);
 	}	
 	
-	/*private static File zipIt(String zipFile, String targetFolder, List<String> fileList) throws Exception{
-
-		LOGGER.entering(GenerateBundle.class.getName(), new Object() {
-		}.getClass().getEnclosingMethod().getName());
-		byte[] buffer = new byte[BUFFER];
-		String source = "";
-		File f = new File(new File(targetFolder).getParent(), zipFile);
-		FileOutputStream fos = null;
-		ZipOutputStream zos = null;
-		try {
-			try {
-				source = targetFolder.substring(targetFolder.lastIndexOf(File.separator) + 1, targetFolder.length());
-			} catch (Exception e) {
-				source = targetFolder;
-			}
-
-			if (f.exists()) {
-				File newFile = new File(f.getParent(),
-						zipFile.substring(0, zipFile.lastIndexOf(".")) + java.lang.System.currentTimeMillis() + ".zip");
-				Files.move(f.toPath(), newFile.toPath());
-			}
-
-			fos = new FileOutputStream(f);
-			zos = new ZipOutputStream(fos);
-
-			LOGGER.fine("Output to Zip : " + zipFile);
-
-			FileInputStream in = null;
-
-			for (String file : fileList) {
-				LOGGER.fine("File Added : " + file);
-				ZipEntry ze = new ZipEntry(source + '/' + file);
-				zos.putNextEntry(ze);
-				try {
-					in = new FileInputStream(targetFolder + '/' + file);
-					int len;
-					while ((len = in.read(buffer)) > 0) {
-						zos.write(buffer, 0, len);
-					}
-				} finally {
-					in.close();
-				}
-			}
-
-			zos.closeEntry();
-			LOGGER.fine("Folder successfully compressed");
-            return f;
-
-		} catch (IOException ex) {
-			LOGGER.severe(ex.getMessage());
-			ex.printStackTrace();
-			throw ex;
-		} finally {
-			try {
-				zos.close();
-			} catch (IOException e) {
-				LOGGER.severe(e.getMessage());
-				e.printStackTrace();
-				throw e;
-			}
-		}
-	}
 	
-	private static List<String> generateFileList(File node, String targetFolder) throws Exception{
-
-        final ArrayList<String> fileList = new ArrayList<>();
-        // add file only
-		if (node.isFile()) {
-			fileList.add(generateZipEntry(node.toString(), targetFolder));
-
-		}
-
-		if (node.isDirectory()) {
-			String[] subNote = node.list();
-			//fileList.add(node.toString());
-			for (String filename : subNote) {
-				fileList.addAll(generateFileList(new File(node, filename), targetFolder));
-			}
-		}
-        return fileList;
-	}
-
-	private static String generateZipEntry(String file, String targetFolder) throws Exception{
-		LOGGER.entering(GenerateBundle.class.getName(), new Object() {
-		}.getClass().getEnclosingMethod().getName());
-		LOGGER.fine("File: " + file + " File Length: " + file.length());
-		LOGGER.fine("Target Folder: " + targetFolder + " Target Folder length: " + targetFolder.length());
-		LOGGER.exiting(GenerateBundle.class.getName(), new Object() {
-		}.getClass().getEnclosingMethod().getName());
-		return file.substring(targetFolder.length() + 1, file.length());
-	}
-	
-	public static File build(String zipFolder, String proxyName) throws Exception{
+	/*public static File build(String zipFolder, String proxyName) throws Exception{
 		LOGGER.entering(GenerateBundle.class.getName(), new Object() {
 		}.getClass().getEnclosingMethod().getName());
         File f;
@@ -140,6 +49,9 @@ public class GenerateBundle {
 	
     private void addFolderToZip(File folder, String parentFolder,
             ZipOutputStream zos) throws FileNotFoundException, IOException {
+		LOGGER.entering(GenerateBundle.class.getName(), new Object() {
+		}.getClass().getEnclosingMethod().getName());
+    	
         for (File file : folder.listFiles()) {
             if (file.isDirectory()) {
                 addFolderToZip(file, parentFolder + "/" + file.getName(), zos);
@@ -163,10 +75,15 @@ public class GenerateBundle {
             zos.closeEntry();
             bis.close();
         }
+		LOGGER.exiting(GenerateBundle.class.getName(), new Object() {
+		}.getClass().getEnclosingMethod().getName());
     }   
     	
 	public File build(String zipFolder, String proxyName) throws Exception {
 		
+		LOGGER.entering(GenerateBundle.class.getName(), new Object() {
+		}.getClass().getEnclosingMethod().getName());
+
 		try {
 			File folder = new File(zipFolder);
 			String parentFolder = zipFolder.substring(zipFolder.lastIndexOf(File.separator) + 1, zipFolder.length());
@@ -180,6 +97,9 @@ public class GenerateBundle {
 			       
 			zos.flush();  
 			zos.close();  
+
+			LOGGER.exiting(GenerateBundle.class.getName(), new Object() {
+			}.getClass().getEnclosingMethod().getName());
 			
 			return f;
 		} catch (Exception e) {
